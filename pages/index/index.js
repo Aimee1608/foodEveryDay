95eabedbbef3c930f96278b4e92f592025389b8b
Aimee1608 <shuigongqian@sina.com>
2017-08-10 00:01:11 +0800

    打的费

--- pages/index/index.js
+++ pages/index/index.js
@@ -73,13 +73,88 @@ Page({
     noMore:true//是否还有更多数据
   },  //事件处理函数
   upper: function (e) {
-      console.log(e)
+      console.log('顶')
   },
   lower: function (e) {
-      console.log(e)
+      console.log('底')
   },
   scroll: function (e) {
       console.log(e)
+  },
+  /**
+  * 生命周期函数--监听页面加载
+  */
+  onLoad: function (options) {
+      var that = this;
+    wx.request({
+        url:'https://h5php.xingyuanauto.com/food/public/index.php/port/food/focus',
+        method: 'GET',
+        // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+        header: {
+            'content-type': 'application/json'
+        },
+        success: function (res) {
+            // success
+            console.log(res);
+        },
+        fail: function (res) {
+            // fail
+            console.log(res);
+        },
+        complete: function () {
+            // complete
+            
+        }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   },
   onReachBottom: function () {
       if(!this.data.noMore){
